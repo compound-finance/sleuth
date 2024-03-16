f63f4051641b37ca1f591d75ecb6adaa739114c8
Geoffrey Hayes <hayesgm@gmail.com>
2024-03-16 13:03:35 -0700

    Get set-up for simple deployments

--- script/deploy-release.sh
+++ script/deploy-release.sh
@@ -9,6 +9,24 @@ if [ -z "$tag" ]; then
   exit 1
 fi
 
+opts=""
+
+if [ -n "$DEPLOYER_PK" ]; then
+  opts="$opts --private-key $DEPLOYER_PK --broadcast"
+fi
+
+if [ -n "$ETHERSCAN_API_KEY" ]; then
+  opts="$opts --verify --etherscan-api-key \"$ETHERSCAN_API_KEY\""
+fi
+
+cleanup() {
+    rv=$?
+    rm -rf .release-tmp
+    exit $rv
+}
+
+trap "cleanup" EXIT
+
 rm -rf .release-tmp
 mkdir .release-tmp
 
@@ -30,7 +48,6 @@ export CODE_JAR="$(cat ./.release-tmp/contracts.json | jq -r '.codeJar')"
 
 forge script \
   --rpc-url="$RPC_URL" \
+  $opts \
   script/Sleuth.s.sol:Deploy \
   $@
-
-# TODO: Verify
